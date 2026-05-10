ee36a411d6c211f6f96d72d739c8cbb4aba25ad8
unknown <alicia.jusup@binus.ac.id>
2026-05-10 16:49:12 +0700

    add progress

--- resources/views/components/ui/progress/progress.blade.php
+++ resources/views/components/ui/progress/progress.blade.php
@@ -1,12 +1,15 @@
-@props(['value' => 0])
+@props([
+    'value' => 0,
+    'color' => 'primary'
+])
 
 <div class="progress">
     <div 
-        class="progress-bar" 
-        role="progressbar" 
-        style="width: {{ $value }}%;" 
-        aria-valuenow="{{ $value }}" 
-        aria-valuemin="0" 
+        class="progress-bar bg-{{ $color }}"
+        role="progressbar"
+        style="width: {{ $value }}%;"
+        aria-valuenow="{{ $value }}"
+        aria-valuemin="0"
         aria-valuemax="100"
     >
         {{ $value }}%
