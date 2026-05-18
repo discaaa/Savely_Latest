7dff4347d6ec435ae3e34f826657632edcccac90
Howard James Winatra <howardjameswinatra@gmail.com>
2026-05-19 02:28:09 +0700

    Create Saving.php

---
+++ app/Models/Saving.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Saving extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'goal_id',
+        'amount',
+        'type',
+        'status',
+        'date',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function goal(): BelongsTo
+    {
+        return $this->belongsTo(Goal::class);
+    }
+}
