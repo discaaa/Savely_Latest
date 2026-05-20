8bbf1d7fcba6e11ef48a6f279ced50a4e19368ab
jol99887 <jolint2007@gmail.com>
2026-05-20 23:06:56 +0700

    fitur expense

--- database/migrations/2026_05_15_153503_create_budgets_table.php
+++ database/migrations/2026_05_15_153503_create_budgets_table.php
@@ -6,25 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('budgets', function (Blueprint $table) {
+
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('expense_categories')->onDelete('cascade');
-            $table->decimal('limit_amount', 15, 2);
-            $table->integer('month');
-            $table->integer('year');
+
+            $table->string('name');
+
+            $table->decimal('total_amount', 15, 2);
+
+            $table->string('period')->default('Monthly');
+
+            $table->date('start_date');
+
             $table->timestamps();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('budgets');
